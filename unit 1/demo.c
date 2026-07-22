e6a0fb5d1fa0033417bfb2a4f8ab04aed5a8a6eb
devyani <jadeja.kishorsinh136498@marwadiuniversity.ac.in>
2026-07-22 12:37:32 +0530

    Add files via upload

---
+++ unit 1/demo.c
@@ -0,0 +1,9 @@
+void main()
+{
+    int a=10;
+    int *p;
+    p=&a;
+    printf("address of a is %d",p);
+    printf("value of a is %d",*p);
+getch();
+}
---
+++ unit 1/p11.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void main()
+{
+    int m1[2][3], m2[3][2], m3[2][2], i, j, k;
+
+    printf("Enter first matrix:\n");
+    for(i=0;i<2;i++)
+        for(j=0;j<3;j++)
+            scanf("%d",&m1[i][j]);
+
+    printf("Enter second matrix:\n");
+    for(i=0;i<3;i++)
+        for(j=0;j<2;j++)
+            scanf("%d",&m2[i][j]);
+
+    for(i=0;i<2;i++)
+        for(j=0;j<2;j++)
+        {
+            m3[i][j]=0;
+            for(k=0;k<2;k++)
+                m3[i][j]+=m1[i][l]*m2[k][j];
+        }
+
+    printf("Result:\n");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+            printf("%d ",m3[i][j]);
+        printf("\n");
+    }
+}
